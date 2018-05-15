ab21f1d5eb55bacb596f2a38f9c4e1c885754831
Lucien Weller <lucien@wellernet.ch>
2018-05-15 15:17:03 +0200

    created dev profile

--- zeus-server/src/main/java/ch/wellernet/zeus/server/service/communication/integrated/drivers/raspberry/MockRaspberryDeviceDriverConfiguration.java
+++ zeus-server/src/main/java/ch/wellernet/zeus/server/service/communication/integrated/drivers/raspberry/MockRaspberryDeviceDriverConfiguration.java
@@ -7,24 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-
 import ch.wellernet.zeus.server.service.communication.integrated.drivers.DeviceDriver;
 
 @Configuration
-@Profile("device.raspberry")
-public class RaspberryDeviceDriverConfiguration {
-
-	@Bean
-	@Lazy
-	public GpioController gpioController() {
-		return GpioFactory.getInstance();
-	}
+@Profile("device.mock")
+public class MockRaspberryDeviceDriverConfiguration {
 
 	@Bean("deviceDriver.raspberry.GpioDigitalOutputPinDriver")
 	@Lazy
 	public DeviceDriver gpioDigitalOutputPin(Properties properties) {
-		return new GpioDigitalOutputPinDriver(properties);
+		return new MockGpioDigitalOutputPinDriver(properties);
 	}
 }
