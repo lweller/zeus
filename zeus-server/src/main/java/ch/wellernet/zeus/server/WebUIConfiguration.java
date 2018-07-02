51c2a166da937af177125cbad3611b02b6d6bbb9
Lucien Weller <lucien@wellernet.ch>
2018-07-02 08:18:15 +0200

    exposing all routes of Web UI

---
+++ zeus-server/src/main/java/ch/wellernet/zeus/server/WebUIConfiguration.java
@@ -0,0 +1,15 @@
+package ch.wellernet.zeus.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebUIConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		registry.addViewController("/devices/**").setViewName("forward:/");
+		registry.addViewController("/events/**").setViewName("forward:/");
+	}
+}
