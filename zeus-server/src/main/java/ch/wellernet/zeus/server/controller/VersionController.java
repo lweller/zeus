50c77abb6d85b579c6a0c6211751ebb1f6f815c6
Lucien Weller <lucien@wellernet.ch>
2018-07-12 19:09:19 +0200

    fixed version controller problem

--- zeus-server/src/main/java/ch/wellernet/zeus/server/controller/VersionController.java
+++ zeus-server/src/main/java/ch/wellernet/zeus/server/controller/VersionController.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.jar.Manifest;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +22,16 @@ import net.minidev.json.JSONObject;
 @RequestMapping("/version")
 public class VersionController {
 
+	private @Autowired ServletContext context;
+
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public JSONObject getVersion() throws IOException {
-		final InputStream inputStream = getClass().getResourceAsStream("META-INF/MANIFEST.MF");
+		InputStream inputStream = context.getResourceAsStream("META-INF/MANIFEST.MF");
+		if (inputStream == null) {
+			// fall back to META-INF ins class path
+			inputStream = getClass().getResourceAsStream("META-INF/MANIFEST.MF");
+		}
 		if (inputStream != null) {
 			return buildVersionInfo(new Manifest(inputStream));
 		}
