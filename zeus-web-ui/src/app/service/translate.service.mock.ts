1e09c2a01e5fdac430ce77248a4410728ec3d9f5
Lucien Weller <lucien@wellernet.ch>
2019-06-10 22:38:49 +0200

    refactor: code improvements

--- zeus-web-ui/src/app/service/translate.service.mock.ts
+++ zeus-web-ui/src/app/service/translate.service.mock.ts
@@ -3,6 +3,7 @@ import {Observable, of} from "rxjs";
 
 @Injectable()
 export class TranslateMockService {
+    // noinspection JSUnusedGlobalSymbols
     public static get(key: any): Observable<any> {
         return of(key);
     }
--- zeus-web-ui/src/environments/environment.prod.ts
+++ zeus-web-ui/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+// noinspection JSUnusedGlobalSymbols
 export const environment = {
     name: 'prod',
     production: true,
