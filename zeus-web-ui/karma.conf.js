3e4a4c54d148d9cfbdc67d77e0ece979fd1426a0
Lucien Weller <lucien@wellernet.ch>
2019-08-22 19:57:02 +0200

    chore: changed karma junit report so that jenkins will find it

--- zeus-web-ui/karma.conf.js
+++ zeus-web-ui/karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
             fixWebpackSourcePaths: true
         },
         junitReporter: {
-            outputFile: 'target/surefire-reports/TESTS-TestSuite.xml',
+            outputFile: 'target/surefire-reports/TEST-AngularJS.xml',
             useBrowserName: false,
             classNameFormatter: function (browser, result) {
                 let name = 'AngularJS.';
