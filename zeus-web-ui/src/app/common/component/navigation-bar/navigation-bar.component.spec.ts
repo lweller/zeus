d2a678394798e13f51cfb50246b21831b7ca7b02
Lucien Weller <lucien@wellernet.ch>
2019-08-26 21:14:20 +0200

    test: added tests vor NavigationBar component

--- zeus-web-ui/src/app/common/component/navigation-bar/navigation-bar.component.spec.ts
+++ zeus-web-ui/src/app/common/component/navigation-bar/navigation-bar.component.spec.ts
@@ -1,7 +1,12 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {async, ComponentFixture, fakeAsync, inject, TestBed, tick} from '@angular/core/testing';
 import {NavigationBarComponent} from './navigation-bar.component';
-import {RouterTestingModule} from "@angular/router/testing";
+import {RouterTestingModule} from '@angular/router/testing';
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
+
+@Component({template: ''})
+class DummyComponent {
+}
 
 describe('NavigationBarComponent', () => {
     let component: NavigationBarComponent;
@@ -9,10 +14,13 @@ describe('NavigationBarComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [NavigationBarComponent],
-            imports: [RouterTestingModule]
-        })
-            .compileComponents().then();
+            declarations: [NavigationBarComponent, DummyComponent],
+            imports: [RouterTestingModule.withRoutes([
+                {path: 'devices', component: DummyComponent},
+                {path: 'events', component: DummyComponent},
+                {path: 'scenarios', component: DummyComponent}
+            ])]
+        }).compileComponents().then();
     }));
 
     beforeEach(() => {
@@ -21,7 +29,42 @@ describe('NavigationBarComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should be created', () => {
-        expect(component).toBeTruthy();
-    });
+    it('should route to devices when devices button is clicked',
+        fakeAsync(inject([Location], (location: Location) => {
+            // given
+            const button = fixture.debugElement.nativeElement.querySelector('#devices-button');
+
+            // when
+            button.click();
+            tick();
+
+            // then
+            expect(location.path()).toBe('/devices');
+        })));
+
+    it('should route to events when events button is clicked',
+        fakeAsync(inject([Location], (location: Location) => {
+            // given
+            const button = fixture.debugElement.nativeElement.querySelector('#events-button');
+
+            // when
+            button.click();
+            tick();
+
+            // then
+            expect(location.path()).toBe('/events');
+        })));
+
+    it('should route to scenarios when scenarios button is clicked',
+        fakeAsync(inject([Location], (location: Location) => {
+            // given
+            const button = fixture.debugElement.nativeElement.querySelector('#scenarios-button');
+
+            // when
+            button.click();
+            tick();
+
+            // then
+            expect(location.path()).toBe('/scenarios');
+        })));
 });
