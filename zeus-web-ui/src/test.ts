07d388206ab436a18f13a137309aefe3cc2b4757
Lucien Weller <lucien@wellernet.ch>
2019-09-04 21:52:04 +0200

    chore: added coverage report to gitignore

--- zeus-web-ui/src/test.ts
+++ zeus-web-ui/src/test.ts
@@ -12,6 +12,7 @@ getTestBed().initTestEnvironment(
     platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
+// noinspection TypeScriptValidateJSTypes
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
